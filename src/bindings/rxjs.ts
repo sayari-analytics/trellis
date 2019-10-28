d949745be2cd4ac5a6bf64cdbf36907e256ce859
James Conkling <james.lane.conkling@gmail.com>
2019-10-28 01:03:28 -0400

    create rxjs bindings

---
+++ src/bindings/rxjs.ts
@@ -0,0 +1,12 @@
+import { Graph, Node, Edge, Options } from '../index'
+import { Observable } from 'rxjs'
+import { switchMap } from 'rxjs/operators'
+
+
+export const observable = (graph: Graph) => {
+  return (props$: Observable<{
+    nodes?: { [key: string]: Node },
+    edges?: { [key: string]: Edge },
+    options?: Partial<Options>
+  }>) => props$.pipe(switchMap(graph.layout))
+}
