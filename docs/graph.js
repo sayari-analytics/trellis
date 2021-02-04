9bcb9759587e2c67310d5e375afe243a427e1beb
Juliet Adams <juliet@gwu.edu>
2021-02-04 10:38:10 -0500

    add test to see if imports worked

---
+++ docs/graph.js
@@ -0,0 +1,7 @@
+import data from './data.js'
+
+const trellis = window.trellis
+const layout = trellis.layout.Force.Layout()
+const renderer = trellis.renderers.WebGL.Renderer
+
+console.log(trellis, layout, renderer)
