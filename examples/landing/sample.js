a183c13f4c3d58b13afcf056bc135e9a9c21d8f5
James Conkling <james.lane.conkling@gmail.com>
2021-02-22 15:50:53 -0500

    shorten example code

--- examples/landing/sample.js
+++ examples/landing/sample.js
@@ -1,17 +1,12 @@
-import * as Force from '../../src/layout/force'
-import * as WebGL from '../../src/renderers/webgl'
+import * as Force from '@sayari/trellis/layout/force'
+import * as WebGL from '@sayari/trellis/renderers/webgl'
 
 /* Initialize Data */
-const NODE_STYLE = {
-  color: '#999',
-  stroke: [{ color: '#FFF', width: 2 }, { color: '#F7CA4D', width: 2 }],
-}
-const EDGE_STYLE = { arrow: 'forward' }
+const NODE_STYLE = { color: '#999', stroke: [{ color: '#FFF', width: 2 }, { color: '#F7CA4D', width: 2 }] }
+const EDGE_STYLE = { arrow: 'forward', width: 2, stroke: '#CCC' }
 let nodes = ['a', 'b', 'c', 'd', 'e', 'f'].map((id) => ({ id, radius: 18, style: NODE_STYLE }))
-let edges = [
-  { id: 'ba', source: 'a', target: 'b' }, { id: 'ca', source: 'a', target: 'c' }, { id: 'da', source: 'a', target: 'd' },
-  { id: 'ce', source: 'c', target: 'e' }, { id: 'cf', source: 'c', target: 'f' }
-].map(({ id, source, target }) => ({ id, source, target, style: EDGE_STYLE }))
+let edges = [['a', 'b'], ['a', 'c'], ['a', 'd'], ['c', 'e'], ['c', 'f']]
+  .map(([source, target]) => ({ id: `${source}_${target}`, source, target, style: EDGE_STYLE }))
 
 /* Create Renderer and Layout */
 const container = document.querySelector('#graph')
@@ -23,26 +18,17 @@ const renderOptions = {
   width: container.offsetWidth,
   height: container.offsetHeight,
   onNodePointerEnter: ({ target: { id } }) => {
-    nodes.forEach((node) => {
-      if (node.id === id) node.style.stroke[1].width = 4
-    })
-
+    nodes.find((node) => node.id === id).style.stroke[1].width = 4
     render({ nodes, edges, options: renderOptions })
   },
   onNodePointerLeave: ({ target: { id } }) => {
-    nodes.forEach((node) => {
-      if (node.id === id) node.style.stroke[1].width = 2
-    })
-
+    nodes.find((node) => node.id === id).style.stroke[1].width = 2
     render({ nodes, edges, options: renderOptions })
   },
   onNodeDrag: ({ nodeX: x, nodeY: y, target: { id } }) => {
-    nodes.forEach((node) => {
-      if (node.id === id) {
-        node.x = x
-        node.y = y
-      }
-    })
+    const node = nodes.find((node) => node.id === id)
+    node.x = x
+    node.y = y
     render({ nodes, edges, options: renderOptions })
   },
   onViewportDrag: ({ viewportX, viewportY }) => {
@@ -55,6 +41,5 @@ const renderOptions = {
 /* Run layout and render graph */
 force({ nodes, edges }).then((graph) => {
   nodes = graph.nodes
-
   render({ nodes, edges, options: renderOptions })
 })
