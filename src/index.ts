141a46c5cecb87b75c14a68a6428c5fc441c9417
Mikey Gower <mgower@sayarianalytics.com>
2023-09-20 18:34:16 -0400

    fix failed build in min bundle

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,92 @@
 export * from './trellis'
-export * from './layout'
-export * from './renderers'
-export * from './bindings'
+
+// layout
+export { default as Cluster } from './layout/cluster'
+export { default as Collide } from './layout/collide'
+export { default as Components } from './layout/components'
+export { default as Fisheye } from './layout/fisheye'
+export { default as Force } from './layout/force'
+export { default as Hierarchy } from './layout/hierarchy'
+export { default as Radial } from './layout/radial'
+
+import type { Options as CollideOptions } from './layout/collide'
+import type { Options as ComponentsOptions } from './layout/components'
+import type { Options as ForceOptions } from './layout/force'
+import type { Options as HierarchyOptions } from './layout/hierarchy'
+import type { Options as RadialOptions } from './layout/radial'
+
+export type { CollideOptions, ComponentsOptions, ForceOptions, HierarchyOptions, RadialOptions }
+
+// renderers
+export { default as ImageRenderer, BlobRenderer } from './renderers/image'
+export { default as Renderer } from './renderers/webgl'
+
+import type { Options as ImageOptions } from './renderers/image'
+import type {
+  NodePointerEvent,
+  NodeDragEvent,
+  EdgePointerEvent,
+  AnnotationPointerEvent,
+  AnnotationResizeEvent,
+  AnnotationDragEvent,
+  AnnotationResizePointerEvent,
+  ViewportPointerEvent,
+  ViewportDragEvent,
+  ViewportDragDecelerateEvent,
+  ViewportWheelEvent,
+  Options as RendererOptions
+} from './renderers/webgl'
+
+export type {
+  ImageOptions,
+  NodePointerEvent,
+  NodeDragEvent,
+  EdgePointerEvent,
+  AnnotationPointerEvent,
+  AnnotationResizeEvent,
+  AnnotationDragEvent,
+  AnnotationResizePointerEvent,
+  ViewportPointerEvent,
+  ViewportDragEvent,
+  ViewportDragDecelerateEvent,
+  ViewportWheelEvent,
+  RendererOptions
+}
+
+// bindings
+export { default as Download } from './bindings/native/download'
+export { default as Selection } from './bindings/native/selection'
+export { default as Zoom } from './bindings/native/zoom'
+export { default as ReactButton } from './bindings/react/button'
+export { default as ReactButtonGroup } from './bindings/react/buttonGroup'
+export { default as ReactRenderer } from './bindings/react/renderer'
+export { default as ReactSelection } from './bindings/react/selection'
+export { default as ReactZoom } from './bindings/react/zoom'
+
+import type { Options as DownloadOptions } from './bindings/native/download'
+import type { Options as ZoomOptions, ViewportChangeOptions } from './bindings/native/zoom'
+import type { Options as SelectionOptions, SelectionChangeEvent } from './bindings/native/selection'
+import type { Props as ButtonProps } from './bindings/react/button'
+import type { Props as ButtonGroupProps } from './bindings/react/buttonGroup'
+import type { Props as RendererProps } from './bindings/react/renderer'
+import type { Props as ZoomProps } from './bindings/react/zoom'
+import type {
+  Props as SelectionProps,
+  ChildProps as SelectionChildProps,
+  SelectionChangeEvent as ReactSelectionChangeEvent
+} from './bindings/react/selection'
+
+export type {
+  DownloadOptions,
+  SelectionOptions,
+  ZoomOptions,
+  ViewportChangeOptions,
+  SelectionChangeEvent,
+  ButtonProps,
+  ButtonGroupProps,
+  RendererProps,
+  SelectionProps,
+  SelectionChildProps,
+  ZoomProps,
+  ReactSelectionChangeEvent
+}
